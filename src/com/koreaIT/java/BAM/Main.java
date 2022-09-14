24afb62b23f5a8b2f1b39d2349004b3a563d73b5
Oyat0824 <langue0824@gmail.com>
2022-09-14 21:42:06 +0900

    게시글 업로드 기능

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -6,15 +6,36 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc= new Scanner(System.in);
-		
+		int contentId = 0;
+				
 		while(true) {
+			
 			System.out.printf("명령어 : ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 			
 			if(cmd.equals("exit")) {
 				break;
+			}
+			
+			if(cmd.equals("")) {
+				System.out.println("명령어를 입력해주세요.");
+				continue;
+			}
+			
+			if(cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			} else if(cmd.equals("article write")) {
+				int id = contentId + 1;
+				contentId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 			} else {
-				System.out.println("명령어가 존재하지 않습니다.");
+				System.out.println("존재하지 않는 명령어 입니다.");
 			}
 		}
 		
@@ -22,3 +43,4 @@ public class Main {
 		sc.close();
 	}
 }
+
